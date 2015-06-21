f88d811441a7900c1fa6b818401a699143adcb6d
hardawaykevin <hardawaykevin@aol.com>
2015-06-21 14:51:18 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Below are two functions (makeCacheMatrix and cacheSolve) to solve inverse matrices problems. 
 
-## Write a short comment describing this function
+## makeCacheMatrix creates the nessassary vectors and functions in order to inverse the matrix 
 
-makeCacheMatrix <- function(x = matrix()) {
+##CacheSolve uses the functions created by makeCacheMatrix to inverse the matrix and complete 
+#problem
 
+
+makeCacheMatrix<- function( x = matrix()){
+        ## set i as inverse vector
+        i <- NULL
+        
+        ## Create the vector for the matrix
+        set <- function(y){ 
+                x<<- y
+                i<<- NULL
+        }
+        ## functions which get and set the matrix and inverse vectors
+        get <- function() x
+        setinverse <- function(inverse) i <<- inverse
+        getinverse<- function() i
+        
+        ## Creates the matrix 
+        list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x,...){
         ## Return a matrix that is the inverse of 'x'
+        ## Assign vector to i
+        i <- x$getinverse()
+        
+        ## check to see if i has a computed value
+        if (!is.null(i)){
+                message("getting cached data")
+                return (i)
+        }
+        
+        ## gathers the data to be inversed
+        data<- x$get()
+        i <- solve(data)
+        
+        ## inverse is cached
+        x$setinverse(i)
+        
+        ## return the inversed matrix 
+        i
 }
